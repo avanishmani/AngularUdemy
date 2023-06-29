c7b6b55433be1d0bbfdf0a0a131aeda71ba2e344
avanishmani <avanishmanitripathi@gmail.com>
2023-06-29 22:14:03 +0530

    Router

---
+++ Section5/RouterModule/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`
+  <div>Hello Angular</div>`,
+})
+export class AppComponent {
+  title = 'RouterModule';
+}
